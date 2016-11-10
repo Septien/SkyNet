61f11cf78bc034b9b644885ea4c25838746e1e64
Jose Septien <jaseptienh@gmail.com>
2016-11-09 18:57:32 -0600

    Agrego tabla usuario.

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,25 @@
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import datetime
 
 #Hazle saber a SQLAlchemy que nuestras clases corresponden a tablas
 Base = declarative_base()
+
+#Tabla usuario
+class Usuario(Base):
+    #Nombre de la tabla
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key = True)
+    nombre = Column(String, nullable = False)
+    apellido = Column(String, nullable = False)
+    fechaDeNacimiento = Column(DateTime)
+    email = Column(String, nullable = False)
+    genero = Column(String(1))
+    contrasena = Column(String, nullable = False)
+    disponibilidad = (Boolean, default = False)
+    conectado = (Boolean, default = False)
+
+
