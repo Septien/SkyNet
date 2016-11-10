6a011fa61f1ac03c8e455006b4f1c6dc6134719b
Jose Septien <jaseptienh@gmail.com>
2016-11-09 18:20:31 -0600

    Agrego instalacion adecuada de MySQL server.

--- pg_config.sh
+++ pg_config.sh
@@ -1,8 +1,13 @@
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 12345'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 12345'
 apt-get -qqy update
-apt-get -qqy install -y mysql-server
+apt-get -qqy install -y mysql-server-5.5
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 apt-get install mysql-client-core-5.5
+apt-get install python-dev libmysqlclient-dev
+pip install -U pip
+pip install MySQL-python
 pip install bleach
 pip install oauth2client
 pip install requests
@@ -12,7 +17,6 @@ pip install passlib
 pip install itsdangerous
 pip install flask-httpauth
 su mysql -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
