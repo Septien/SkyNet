fe0189d296392ae6c0a72f15449faac9b2978a9b
Jose Septien <jaseptienh@gmail.com>
2016-11-15 13:29:38 -0600

    Agrego punto para ejecutar el servidor.

--- skynet_server.py
+++ skynet_server.py
@@ -16,3 +16,8 @@ session = DBSession()
 
 #Inicializa la app de Flask
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
