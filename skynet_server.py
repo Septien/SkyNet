d931e9e37756713e44355e11d21246e52e341ee3
Jose Septien <jaseptienh@gmail.com>
2016-11-30 19:36:45 -0600

    Add function of index.

--- skynet_server.py
+++ skynet_server.py
@@ -1,14 +1,15 @@
+import sys
 #Modulos necesarios para usar Flask
 from flask import Flask, render_template, url_for, request, redirect, flash
 #Modulos necesario para usar SQLAlchemy y base de datos
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Usuario, Fotos, Publicación, Etiquetas, Amigos, Chat, Mensaje
+import database_setup as db
 
 #Crear motor de base de datos
 engine = create_engine("mysql+pymysql://root:12345@localhost/")
 engine.execute("USE skynet")
-Base.metadata.bind = engine
+db.Base.metadata.bind = engine
 
 #Crear sesion para comunicarse con la base de datos
 DBSession = sessionmaker(bind = engine)
@@ -16,6 +17,11 @@ session = DBSession()
 
 #Inicializa la app de Flask
 app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
