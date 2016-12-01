c9eef2854de2acb47e63c17fa820550419872382
Jose Septien <jaseptienh@gmail.com>
2016-12-01 13:41:07 -0600

    Add proper separation and comment. Add proper relationships among tables.

--- database_setup.py
+++ database_setup.py
@@ -14,7 +14,10 @@ Base = declarative_base()
 class Usuario(Base):
     #Nombre de la tabla
     __tablename__ = 'usuario'
+    #Primary key
     id = Column(Integer, primary_key = True)
+
+    #User data
     nombre = Column(String(40), nullable = False)
     apellido = Column(String(40), nullable = False)
     fechaDeNacimiento = Column(DateTime)
@@ -22,44 +25,59 @@ class Usuario(Base):
     email = Column(String(40), nullable = False)
     genero = Column(String(1))
     contrasena = Column(String(16), nullable = False)
-    disponibilidad = Column(Boolean, default = False)
-    conectado = Column(Boolean, default = False)
+    disponibilidad = Column(Boolean, default = False)   #On chat
+    conectado = Column(Boolean, default = False)        #To server
 
 #Tabla de fotos
 class Fotos(Base):
     __tablename__ = 'fotos'
+    #Primary key
     id = Column(Integer, primary_key = True)
-    usuario = relationship(Usuario)
+
+    #Foregin key
     uid = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+
+    #The image
     img = Column(BLOB)
 
 #Tabla Publicacion
 class Publicacion(Base):
     __tablename__ = 'publicacion'
+    #Primary key
     id = Column(Integer, primary_key = True)
+
+    #Foreign key
+    uid = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+    fid = Column(Integer, ForeignKey('fotos.id'))
+    fotos = relationship(Fotos)
+
+    #Needed fields
     texto = Column(TEXT)
     #http://stackoverflow.com/questions/6992321/how-to-set-default-on-update-current-timestamp-in-mysql-with-sqlalchemy
     fecha = Column(TIMESTAMP, server_default = text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
     num_likes = Column(Integer, default = 0)
-    usuario = relationship(Usuario)
-    uid = Column(Integer, ForeignKey('usuario.id'))
-    fotos = relationship(Fotos)
-    fid = Column(Integer, ForeignKey('fotos.id'))
 
 #Tabla de etiquetas
 class Etiquetas(Base):
     __tablename__ = 'etiquetas'
-    publicacion = relationship(Publicacion)
+    #Primary and foreign keys
     pid = Column(Integer, ForeignKey('publicacion.id'), primary_key = True)
-    usuario = relationship(Usuario)
+    publicacion = relationship(Publicacion)
     uid = Column(Integer, ForeignKey('usuario.id'), primary_key = True)
+    usuario = relationship(Usuario)
 
 #Tabla de amigos
 class Amigos(Base):
     __tablename__ = 'amigos'
-    usuario = relationship(Usuario)
+    #Foregin and primary keys
     uid = Column(Integer, ForeignKey('usuario.id'), primary_key = True)
     amigo_id = Column(Integer, ForeignKey('usuario.id'), primary_key = True)
+    usuario = relationship(Usuario, foreign_keys = [uid])
+    amigo = relationship(Usuario, foreign_keys = [amigo_id])
+
+    #Information needed
     permiso_compartir = Column(Boolean, default = True)
     ver_contenido = Column(Boolean, default = True)
     bloqueado = Column(Boolean, default = False)
@@ -68,23 +86,36 @@ class Amigos(Base):
 #Tabla de chat
 class Chat(Base):
     __tablename__ = 'chat'
+    #Primary keys
     id = Column(Integer, primary_key = True)
-    usuario = relationship(Usuario)
+
+    #Foreign keys
     uid = Column(Integer, ForeignKey('usuario.id'))
     amigo_id = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario, foreign_keys = [uid])
+    amigo = relationship(Usuario, foreign_keys = [amigo_id])
 
 #Tabla de mensajes de chat
 class Mensaje(Base):
     __tablename__ = 'mensaje'
+    #Primary key
     id = Column(Integer, primary_key = True)
-    chat = relationship(Chat)
+
+    #Foreign key to chat
     cid = Column(Integer, ForeignKey('chat.id'))
-    usuario = relationship(Usuario)
+    chat = relationship(Chat)
+
+    #Foreign key to user
     uid = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+
+    #Foreign key to images
+    iid = Column(Integer, ForeignKey('fotos.id'))
+    fotos = relationship(Fotos)
+
+    #Needed information
     texto = Column(TEXT)
     fecha = Column(TIMESTAMP, server_default = text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
-    fotos = relationship(Fotos)
-    iid = Column(Integer, ForeignKey('fotos.id'))
     ultimo = Column(Boolean, default = True)
 
 #http://stackoverflow.com/questions/22252397/importerror-no-module-named-mysqldb
