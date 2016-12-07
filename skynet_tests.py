ccf2be8c837bb6bed452bb6efeaee4a7e7e19901
Jose Septien <jaseptienh@gmail.com>
2016-12-07 11:07:25 -0600

    Add test for the function index.

--- skynet_tests.py
+++ skynet_tests.py
@@ -31,5 +31,47 @@ class SkynetTestCase(unittest.TestCase):
 	def tearDown(self):
 		pass
 
+		def login(self, email, password, route):
+		"""For testing the log in"""
+		return self.app.post(route, data = dict(email = email, password = password), follow_redirects = True)
+
+	def test_index(self):
+		"""
+		Test the method index. If the method of the request is GET, just verify that the returned HTML
+		is correctly formatted. Check if the string 'SKYNET' is present in the file.
+		If the methos is POST, there are many things to verify:
+			-If there is no password or email on the request form, indicate that a field is missing.
+			-If the format of the email is incorrect, indicate to the user that email format is wrong
+			-If the user is not in the database or the password is incorrect: indicate it to the user.
+			-If all went fine, check that the status of the user is correctly updated on the data base and the 
+			returned template is the adecuate.
+		"""
+		#Tests for GET method
+		rv = self.app.get("/")
+		assert "SKYNET" in rv.data
+		rv = self.app.get("/index")
+		assert "SKYNET" in rv.data
+
+		#Tests for POST method
+		#Testing log in
+		rv = self.login("johns@correo.com", "", "/")
+		assert "Missing field" in rv.data
+		rv = self.login("", "12345", "/")
+		assert "Missing field" in rv.data
+		rv = self.login("johns", "12345", "/")
+		assert "Incorrect email format" in rv.data
+		rv = self.login("john@correo.com", "12345", "/")
+		assert "User not registered or incorrect password" in rv.data
+		rv = self.login("johns@correo.com", "1234", "/")
+		assert "User not registered or incorrect password" in rv.data
+
+		rv = self.login("johns@correo.com", "12345", "/")
+		#Get the user from the database
+		user = self.session.query(Usuario).filter(Usuario.email == "johns@correo.com").one()
+		assert user.conectado == True
+		assert user.disponibilidad == True
+		#Test correct redirect
+		assert "John Snow" in rv.data
+
 if __name__ == '__main__':
 	unittest.main()
