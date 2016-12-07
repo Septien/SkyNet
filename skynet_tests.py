76c4ca1abe98a96ce8c496a3e9d6cc83a6e65698
Jose Septien <jaseptienh@gmail.com>
2016-12-07 12:49:10 -0600

    Add function to test register.

--- skynet_tests.py
+++ skynet_tests.py
@@ -77,8 +77,64 @@ class SkynetTestCase(unittest.TestCase):
 		#Test correct redirect
 		assert "johns" in rv.data
 
+	def register(self, name, lastname, email, password):
+		return self.app.post("/register", data = dict(name = name, LastName = lastname, email = email, \
+			password = password), follow_redirects = True)
+
 	def test_register(self):
-		pass
+		"""
+		Test the method register. If the method of the request is GET, just verify that the returned HTML
+		is correctly formatted. Check if the strings 'First Name', 'Last Name', 'Your Email', 'Password'
+		and 'Register' are present in the file.
+		If the methos is POST, there are many things to verify:
+			-If there is no first name, last name, email or password on the request form, indicate that a field is missing.
+			-If the format of the email is incorrect, indicate to the user that email format is wrong
+			-If the user is already in the database: indicate it to the user.
+			-If all went fine, check that the user is correctly added to the database and the
+			status of the user is correctly updated on the data base and the returned template is the adecuate.
+		"""
+		session = self.DBSession()
+		#Test the GET method
+		rv = self.app.get("/register")
+		assert "First Name" in rv.data
+		assert "Last Name" in rv.data
+		assert "Your Email" in rv.data
+		assert "Password" in rv.data
+		assert "Register" in rv.data
+
+		#Test the POST method
+		#Check for missing fields
+		rv = self.register("", "", "", "")
+		assert "Missing field" in rv.data
+		rv = self.register("Angry", "Bird", "blue_bird@angry.ab", "")
+		assert "Missing field" in rv.data
+		rv = self.register("Angry", "Bird", "", "angryme")
+		assert "Missing field" in rv.data
+		rv = self.register("Angry", "", "blue_bird@angry.ab", "angryme")
+		assert "Missing field" in rv.data
+		rv = self.register("", "Bird", "blue_bird@angry.ab", "angryme")
+		assert "Missing field" in rv.data
+
+		#Incorrect email format
+		rv = self.register("Angry", "Bird", "blue_bird@angry", "angryme")
+		assert "Incorrect email format" in rv.data
+
+		#Already registered user
+		rv = self.register("John", "Snow", "johns@correo.com", "12345")
+		assert "User already register" in rv.data
+
+		#Check correct add of user to database and update status
+		rv = self.register("Angry", "Bird", "blue_bird@angry.ab", "angryme")
+		q = session.query(exists().where(Usuario.username == "blue_bird")).scalar()
+		assert q == True
+		user = session.query(Usuario).filter(Usuario.email == "blue_bird@angry.ab").one()
+		assert user.nombre == "Angry"
+		assert user.apellido == "Bird"
+		assert user.email == "blue_bird@angry.ab"
+		assert user.contrasena == "angryme"
+		assert user.username == "blue_bird"
+		assert user.conectado == True
+		assert user.disponibilidad == True
 
 
 if __name__ == '__main__':
